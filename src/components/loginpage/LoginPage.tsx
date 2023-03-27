88560f36ffe1990c5e575f66b76abe1b07a26d26
IDDQD404 <ebatkobanchick@gmail.com>
2023-03-27 22:59:32 +0300

    Removed useless comment

--- src/components/loginpage/LoginPage.tsx
+++ src/components/loginpage/LoginPage.tsx
@@ -20,23 +20,6 @@ export default function LoginPage() {
     password: "password",
   });
 
-  /*
-  function getPromise(URL) {
-  let promise = new Promise(function (resolve, reject) {
-    let req = new XMLHttpRequest();
-    req.open("GET", URL);
-    req.onload = function () {
-      if (req.status == 200) {
-        resolve(req.response);
-      } else {
-        reject("There is an Error!");
-      }
-    };
-    req.send();
-  });
-  return promise;
-}
-  */
   async function LogIn() {
     await fetch(
       `${process.env.REACT_APP_API_URL}/ru/data/v3/testmethods/docs/login`,
